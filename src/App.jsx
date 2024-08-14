ecfbbee4e9589ef24b06192251c5d78a72b77765
Ayman ezz <aymanmae12@gmail.com>
2024-08-14 15:40:04 +0300

    add Highlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Modal from "./components/Modal";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Hero/>
       <Highlights/>
+      <Modal/>
     </main>
   )
 }
